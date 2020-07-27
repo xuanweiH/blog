5bb0e48f86cb224a3e11c20c99d0c20c98fa4290
xuanweiH <525405891@qq.com>
2020-07-27 11:24:55 +0800

    迭代翻转链表.js

--- algorithm/链表/翻转链表.js
+++ algorithm/链表/翻转链表.js
@@ -5,12 +5,35 @@
 // 进阶:
 // 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
 
-// 递归 指针从后指向前 
+
+// 迭代
+var reverse = function() {
+   // 边界条件
+   if (!head || !head.next) return head
+   let prev = null, curr = head
+   while (curr) {
+       //  临时存储当前节点的后继节点
+       let next = curr.next
+       //  把后继指针翻转
+       curr.next = prev
+       // 变更节点 prev curr
+       prev = curr
+       curr = next
+   }
+   head = prev
+   return head
+}
+
+
+
+
+// 递归 指针从后指向前 不断翻转当前节点的后继节点next
 var reverseList = function(head) {
-    if(!head || !head.next) return head
-    var next = head.next
+    // 边界条件 没有后继节点了
+    if (!head || !head.next) return head
+    let next = head.next
     // 递归反转
-    var reverseHead = reverseList(next)
+    let reverseHead = reverseList(next)
     // 变更指针
     next.next = head
     head.next = null
