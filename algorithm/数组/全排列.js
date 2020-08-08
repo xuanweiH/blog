4128e2380cc112734fb480d7bb1a1d2de51f0109
xuanweiH <525405891@qq.com>
2020-08-08 16:16:35 +0800

    全排列

--- algorithm/数组/全排列.js
+++ algorithm/数组/全排列.js
@@ -1,22 +1,30 @@
-function allSort (nums) {
-    let n = nums.length;
-    let res = [];
-    let tmpPath = [];
-    let backtrack = (tmpPath) => {
-        if(tmpPath.length == n){
-            res.push(tmpPath);
-            return;
-        }
-        for(let i = 0;i < n;i++){
-            if(!tmpPath.includes(nums[i])){
-                tmpPath.push(nums[i]);
-                backtrack(tmpPath.slice());
-                tmpPath.pop();
-            }
-        }
-    }
-    backtrack(tmpPath);
-    return res;
-}
+// 全排列 输入一个数组 例如[a,b,c] 输出所有不重复的的情况
+// [a,b,c] [a,c,b] [b,a,c] [b,c,a] [c,a,b] [c,b,a]
 
+// 思路: 回溯递归
+// 操作当前层完成后 讲结果继续向下层递归
+// 每当这一层的操作完成后,就会返回上一层的其他情况继续遍历
+
+
+function permutations (nums) {
+   let len = nums.length
+   let res = []
+   let tmpPath = []
+   let dfs = (tmpPath) => {
+      if (tmpPath.length === len) {
+          res.push(tmpPath)
+          return 
+      }
+      for(let i=0;i<len;i++) {
+          if(!tmpPath.includes(nums[i])) {
+              tmpPath.push(nums[i])
+              dfs(tmpPath)
+              tmpPath.pop()
+          }
+      }
+   }
+   dfs(tmpPath)
+   return res
+
+}
 // console.log([1,2,3].slice())
