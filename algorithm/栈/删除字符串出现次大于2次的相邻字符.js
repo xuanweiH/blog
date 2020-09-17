f5c295a0c643a0e1dd7941c0fe9417bf639d249d
xuanweiH <525405891@qq.com>
2020-09-17 12:45:56 +0800

    复习删除字符串出现大于2次的相邻字符

--- algorithm/栈/删除字符串出现次大于2次的相邻字符.js
+++ algorithm/栈/删除字符串出现次大于2次的相邻字符.js
@@ -3,7 +3,7 @@
 // 解释："abbbaca" => "aaca"=>"ca"
 
 
-//思路 首先找到栈顶的值 已经当前值
+//思路 首先找到栈顶的值 以及当前值
 // 如果栈顶值等于当前值 就把栈顶值弹出 并且如果继续循环如果还是相等就i++
 function del(s) {
   const stack = []
@@ -23,4 +23,25 @@ function del(s) {
   }
   return stack.join('')
 }
-console.log(del("abbbaca"))
+console.log(del("abb"))
+
+
+
+
+// function del (str) {
+//   let stack = []
+//   let i =0 
+//   while(i<str.length) {
+//     let next = str[i]
+//     let top = stack[stack.length-1]
+//     if (top === next ) {
+//       stack.pop()
+//       while(str[i]==top) i++
+ 
+//     } else {
+//       stack.push(next)
+//       i++
+//     }
+//   }
+//   return stack.join('')
+// }console.log(del("abb"))
