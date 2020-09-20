14c1ffcf9dd0f8f3e60cc2a3dc742a54fda08d36
xuanweiH <525405891@qq.com>
2020-09-20 14:21:10 +0800

    归并排序思路

---
+++ algorithm/排序/归并排序.js
@@ -0,0 +1,50 @@
+// 归并算法  merge_sort
+// 核心思路  将一个数组分为两个子数组,
+// 通过递归重复将数组切分到只剩下一个元素为止,
+// 然后将每个子数组中的最大元素排序后合并,
+// 通过不断合并子数组,最后就会拿到一个排好序的大数组.
+
+// 分而治之
+
+// 递归实现步骤
+// 1.递归切分当前数组
+// 2.如果当前数组数量少于等于1 无需排序,直接返回结果
+// 3.否则将当前数组分为两个子数组,递归排序这个两个子数组
+// 4.在子数组排序结束后,将子数组的结果归并为排好序的数组
+
+function mergeSort (arr) {
+    let len = arr.length
+    if (len < 2) {
+        return arr
+    }
+    let middle = Math.floor(len/2)
+    //拆分成两个子数组
+    let left =  arr.slice(0, middle)
+    let right = arr.slice(middle,len)
+    //递归拆分
+    let mergeSortLeft = mergeSort(left)
+    let mergeSortRight = mergeSort(right)
+    //合并
+    return merge( mergeSortLeft,mergeSortRight)
+}
+const merge = (left, right) => {
+    const result = [];
+
+    while (left.length && right.length) {
+        // 注意: 判断的条件是小于或等于，如果只是小于，那么排序将不稳定.
+        if (left[0] <= right[0]) {
+            result.push(left.shift()); //每次都要删除left或者right的第一个元素，将其加入result中
+        } else {
+            result.push(right.shift());
+        }
+    }
+    //将剩下的元素加上
+    while (left.length) result.push(left.shift());
+
+    while (right.length) result.push(right.shift());
+
+    return result;
+};
+let arr = [1,4,2,5,3,6]
+console.log(mergeSort(arr))
+console.log(arr)
