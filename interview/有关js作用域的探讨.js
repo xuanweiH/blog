a323f89a9ad377f08d3538dc883af4f677a334e0
xuanweiH <525405891@qq.com>
2020-10-26 17:33:30 +0800

    有关js作用域的一道题

---
+++ interview/有关js作用域的探讨.js
@@ -0,0 +1,18 @@
+// {
+//     a = 50
+//     console.log(a) // 50
+// }
+
+// console.log(a) // a is not define
+// {
+//     a = 50
+// }
+
+// {
+//     a = 50
+//     console.log(window.a)
+// }
+var a = 50
+console.log(window.a)
+
+
