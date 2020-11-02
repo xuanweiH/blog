364e15514433fb5a4cada69319426a5c1c6ccb30
xuanweiH <525405891@qq.com>
2020-11-02 11:19:39 +0800

    归并算法练习

--- algorithm/排序/归并排序.js
+++ algorithm/排序/归并排序.js
@@ -12,39 +12,76 @@
 // 3.否则将当前数组分为两个子数组,递归排序这个两个子数组
 // 4.在子数组排序结束后,将子数组的结果归并为排好序的数组
 
-function mergeSort (arr) {
-    let len = arr.length
-    if (len < 2) {
-        return arr
-    }
-    let middle = Math.floor(len/2)
-    //拆分成两个子数组
-    let left =  arr.slice(0, middle)
-    let right = arr.slice(middle,len)
-    //递归拆分
-    let mergeSortLeft = mergeSort(left)
-    let mergeSortRight = mergeSort(right)
-    //合并
-    return merge( mergeSortLeft,mergeSortRight)
+// function mergeSort (arr) {
+//     let len = arr.length
+//     if (len < 2) {
+//         return arr
+//     }
+//     let middle = Math.floor(len/2)
+//     //拆分成两个子数组
+//     let left =  arr.slice(0, middle)
+//     let right = arr.slice(middle,len)
+//     //递归拆分
+//     let mergeSortLeft = mergeSort(left)
+//     let mergeSortRight = mergeSort(right)
+//     //合并
+//     return merge( mergeSortLeft,mergeSortRight)
+// }
+// const merge = (left, right) => {
+//     const result = [];
+
+//     while (left.length && right.length) {
+//         // 注意: 判断的条件是小于或等于，如果只是小于，那么排序将不稳定.
+//         if (left[0] <= right[0]) {
+//             result.push(left.shift()); //每次都要删除left或者right的第一个元素，将其加入result中
+//         } else {
+//             result.push(right.shift());
+//         }
+//     }
+//     //将剩下的元素加上
+//     while (left.length) result.push(left.shift());
+
+//     while (right.length) result.push(right.shift());
+
+//     return result;
+// };
+// let arr = [1,4,2,5,3,6]
+// console.log(mergeSort(arr))
+// console.log(arr)
+
+
+
+
+/// 归并排序 练习 
+// 思路 递归拆分 递归合并
+// 拆分
+// [1,4,2,5]  [1,4] [2,5] [1][4][2][5]
+function mergeSort(arr) {
+  let len = arr.length
+  if(len < 2) return arr
+  let partIndex = Math.floor(len/2)
+  let left = arr.slice(0,partIndex)
+  let right = arr.slice(partIndex,len)
+  let leftRes = mergeSort(left)
+  let rigtRes = mergeSort(right)
+  return merge(leftRes,rigtRes)
 }
-const merge = (left, right) => {
-    const result = [];
-
-    while (left.length && right.length) {
-        // 注意: 判断的条件是小于或等于，如果只是小于，那么排序将不稳定.
-        if (left[0] <= right[0]) {
-            result.push(left.shift()); //每次都要删除left或者right的第一个元素，将其加入result中
-        } else {
-            result.push(right.shift());
-        }
-    }
-    //将剩下的元素加上
-    while (left.length) result.push(left.shift());
-
-    while (right.length) result.push(right.shift());
-
-    return result;
-};
-let arr = [1,4,2,5,3,6]
+// 合并
+function merge(left, right) {
+  console.log('left',left)
+  console.log('right',right)
+  const res = []
+  while(left.length && right.length) {
+     if (left[0]>right[0]) {
+         res.push(right.shift())
+     } else {
+         res.push(left.shift())
+     }
+  } 
+  while(left.length) res.push(left.shift())
+  while(right.length) res.push(right.shift())
+  console.log(res)
+  return res 
+}
+let arr = [1,4,2,5]
 console.log(mergeSort(arr))
-console.log(arr)
