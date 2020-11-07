bf18053c201156f188b382ea1a25442ca3576dd7
xuanweiH <525405891@qq.com>
2020-11-07 15:52:14 +0800

    二分查找之数组查找元素的第一个和最后一个位置复习

--- algorithm/查找/在排序数组中查找元素的第一个和最后一个位置.js
+++ algorithm/查找/在排序数组中查找元素的第一个和最后一个位置.js
@@ -13,6 +13,58 @@
 // 输入: nums = [5,7,7,8,8,10], target = 6
 // 输出: [-1,-1]
 
+// 查找排序数组中 某个元素第一个和最后一个的位置
+function search (nums, target) {
+  return [searchLseft(nums,target),searchRight(nums, target)]
+}
+function searchLseft(nums, target) {
+  let low = 0, high = nums.length -1
+  let mid
+  while(low<=high) {
+    mid = Math.floor((low+high)/2)
+    if (nums[mid]< target) {
+      low = mid + 1
+    } else if (nums[mid]>target) {
+      high = mid -1
+    } else {
+      high = mid -1
+    }
+  }
+  if (low>nums.length || nums[low]!==target) return -1
+  return low
+}
+function searchRight(nums, target) {
+  let low = 0, high = nums.length -1
+  let mid
+  while(low<=high) {
+    mid = Math.floor((low+high)/2)
+    if (nums[mid]< target) {
+      low = mid + 1
+    } else if (nums[mid]>target) {
+      high = mid -1
+    } else {
+      low = mid +1
+    }
+  }
+  if (high<0 || nums[high]!==target) return -1
+  return high
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 思路: 二分查找 从
 function findItem(nums, target) {
   return [leftSearch(nums, target),rightSearch(nums, target)]
@@ -33,7 +85,7 @@ function leftSearch(nums, target) {
           high = mid -1
       } else {
           // 相等的时候 取到右边界
-          high = mid - 1
+        high = mid +1
       }
   }
   if (low>=nums.length || nums[low] !== target) return -1
