8573ef2bc3b5ce5f32499214e294d3adbc53093c
xuanweiH <525405891@qq.com>
2020-11-09 14:51:26 +0800

    evenloop 有关练习

---
+++ interview/evenloop/test.js
@@ -0,0 +1,32 @@
+
+function executor(resolve, reject) {
+    let rand = Math.random();
+    console.log(1)
+    console.log(rand)
+    if (rand > 0.5)
+        resolve()
+    else
+        reject()
+}
+var p0 = new Promise(executor); // 1 rand 2 s1 1 rand 2
+var p1 = p0.then((value) => {
+    console.log("succeed-1")
+    return new Promise(executor)
+})
+
+
+var p3 = p1.then((value) => {
+    console.log("succeed-2")
+    return new Promise(executor)
+})
+
+var p4 = p3.then((value) => {
+    console.log("succeed-3")
+    return new Promise(executor)
+})
+
+
+p4.catch((error) => {
+    console.log("error")
+})
+console.log(2)
