703bfa227b341c1b7bc5a5e3b2c1a92763a175fa
xuanweiH <525405891@qq.com>
2021-01-04 18:52:33 +0800

    两数之和复习

--- algorithm/数组/两数之和.js
+++ algorithm/数组/两数之和.js
@@ -8,18 +8,38 @@
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1] */
 
-解题思路:
+// 解题思路:
 // 可以利用js的map结构, 把nums[i]作为map的key index作为value
 // 如果目标值-当前值在 map的key中存在 说明这两个数之和为target
 // 取出差值对应的下标以及当前下标
 
-function sums (nums, target) {
-   let map = new Map()
-   for (let i = 0; i<nums.lenth; i++) {
-       let k = target - nums[i]
-       if (map.has(k)) {
-           return [map.get(k), i]
-       }
-       map.set(nums[i], i)
-   }
+function twoSum(nums, target) {
+    let map = new Map()
+    for(let i=0;i<nums.length;i++) {
+        let k = target - nums[i]
+        if(map.has(k)) {
+            return [map.get(k), i]
+        }
+        map.set(nums[i], i)
+
+    }
 }
+
+
+
+
+
+
+
+// function sums (nums, target) {
+//    let map = new Map()
+//    for (let i = 0; i<nums.lenth; i++) {
+//        let k = target - nums[i]
+//        if (map.has(k)) {
+//            return [map.get(k), i]
+//        }
+//        map.set(nums[i], i)
+//    }
+// }
+
+
