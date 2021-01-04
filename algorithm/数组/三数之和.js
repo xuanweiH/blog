260bb0702733ffc3b3219ee965eae1660813bdea
xuanweiH <525405891@qq.com>
2021-01-04 19:07:34 +0800

    三数之和复习

--- algorithm/数组/三数之和.js
+++ algorithm/数组/三数之和.js
@@ -13,6 +13,46 @@
 // 思路 判断这个数组 如果少于三个元素那肯定不存在
 // 给数组排个序 再去个重
 // 找到第二个数,最后一个数,再用双指针平推
+function threeSum(nums) {
+  if (nums.length<3) return []
+  nums.sort((a,b) => a-b)
+  let res = [], second, last
+  for (let i=0;i<nums.length;i++) {
+    if (nums[i]>0) break
+     if(i>0 && nums[i-1] === nums[i]) continue
+     second = i+1
+     last = nums.length -1
+     while(second < last) {
+       let sum = nums[i] + nums[second] + nums[last]
+       if (!sum) {
+         res.push([nums[i],nums[second], nums[last]])
+         while(second<last && nums[second+1]===nums[second]) second++
+         while(second<last && nums[last-1] ===nums[last]) last--
+         second++
+         last--
+       } else {
+        if (sum > 0) second++
+        if (sum<0) last--
+       }
+     }
+  }
+  return res
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function threeSums (nums) {
