c2da410fb0d858a32d0dd4ceecfdbd393ab04527
xuanweiH <525405891@qq.com>
2021-01-05 11:02:53 +0800

    三数之和复习+1

--- algorithm/数组/三数之和.js
+++ algorithm/数组/三数之和.js
@@ -13,33 +13,64 @@
 // 思路 判断这个数组 如果少于三个元素那肯定不存在
 // 给数组排个序 再去个重
 // 找到第二个数,最后一个数,再用双指针平推
-function threeSum(nums) {
-  if (nums.length<3) return []
-  nums.sort((a,b) => a-b)
-  let res = [], second, last
-  for (let i=0;i<nums.length;i++) {
-    if (nums[i]>0) break
-     if(i>0 && nums[i-1] === nums[i]) continue
-     second = i+1
-     last = nums.length -1
-     while(second < last) {
-       let sum = nums[i] + nums[second] + nums[last]
-       if (!sum) {
-         res.push([nums[i],nums[second], nums[last]])
-         while(second<last && nums[second+1]===nums[second]) second++
-         while(second<last && nums[last-1] ===nums[last]) last--
+
+// function threeSum(nums) {
+//   if (nums.length<3) return []
+//   nums.sort((a,b) => a-b)
+//   let res = [], second, last
+//   for (let i=0;i<nums.length;i++) {
+//     if (nums[i]>0) break
+//      if(i>0 && nums[i-1] === nums[i]) continue
+//      second = i+1
+//      last = nums.length -1
+//      while(second < last) {
+//        let sum = nums[i] + nums[second] + nums[last]
+//        if (!sum) {
+//          res.push([nums[i],nums[second], nums[last]])
+//          while(second<last && nums[second+1]===nums[second]) second++
+//          while(second<last && nums[last-1] ===nums[last]) last--
+//          second++
+//          last--
+//        } else {
+//         if (sum < 0) second++
+//         if (sum>0) last--
+//        }
+//      }
+//   }
+//   return res
+// }
+
+
+// 三数之和 解题思路
+// nums 先把这个数组排序
+// 然后判断是否长度大于3
+// 然后进行判断 是否有重复项 有重复项去除
+// 两个指针 second和last 推移 然后再去重
+
+function threeSums(nums) {
+   if (nums.length<3) return []
+   nums.sort((a,b) => a-b)
+   let res = [], second, last
+   for(let i=0;i<nums.length;i++) {
+    if(i>0 && nums[i-1] === nums[i]) continue
+    second = i+1
+    last= nums.length-1
+    while(second<last) {
+      let sum = nums[i] + nums[second] + nums[last]
+      if (!sum) {
+         res.push([nums[i], nums[second], nums[last]])
+         while(second<last && nums[second] === nums[second+1]) second++
+         while(second<last && nums[last] === nums[last-1]) last--
          second++
          last--
-       } else {
-        if (sum > 0) second++
-        if (sum<0) last--
-       }
-     }
-  }
-  return res
+      } else {
+        if(sum>0) last--
+        if(sum<0) second++
+      }
+    }
+   }
+   return res
 }
-
-
 
 
 
@@ -73,8 +104,8 @@ function threeSums (nums) {
              second++
              last--
            } else {
-              if (sum > 0) second++
-              if (sum<0) last--
+              if (sum < 0) second++
+              if (sum > 0) last--
            }
        }
    }
