4c067128c8cfa991050d962d60f846272a6468f6
xuanweiH <525405891@qq.com>
2021-01-07 19:00:51 +0800

    d多个数组求交集

--- algorithm/数组/多个数据求交集.js
+++ algorithm/数组/多个数据求交集.js
@@ -4,10 +4,28 @@
 // 判断前一个数组中的项是否存在后一个数组中,存在则留下
 // reduce依次后推到达效果
 
-function interSection (...args) {
+function interSection (args) {
     if (args.length === 0) return []
     if (args.length === 1) return [args[0]]
     return [...new Set(args.reduce((pre,cur) => {
         return pre.filter(item => cur.include(item))
+    }))]
+}
+
+
+
+
+
+
+
+
+
+
+// 多个数组求交集练习
+function getsum(args) {
+    if(args.length === 0) return
+    if(args.length === 1) return [args[0]]
+    return [...new Set(args.reduce((pre, cur) => {
+        return pre.filter(item => cur.includes(item))
     }))]
 }
