eeb588fba7ea6a60020064b73afb8bdbe7c297f9
xuanweiH <525405891@qq.com>
2021-02-03 16:53:24 +0800

    有效三角形个数复习

--- algorithm/数组/有效三角形的个数.js
+++ algorithm/数组/有效三角形的个数.js
@@ -64,4 +64,24 @@ function triangleCount(nums) {
   return count
 }
 
-console.log(triangleCount([2,2,3,4]))
+console.log(triangleCount([3,5,5,6,7]))
+
+
+
+function getTriangle(nums) {
+    if(!nums || nums.length<3) return 0
+    let count = 0
+    nums.sort((a,b) => a-b)
+    for(let k= nums.length-1;k>1;k--) {
+        let i=0, j=k-1
+        while(i<j) {
+            if(a[i] + a[j] > a[k]) {
+                count+= j-1
+                j--
+            }else {
+                i++
+            }
+        }
+    }
+    return count
+}
