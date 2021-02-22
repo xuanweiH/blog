8e695513ab2cae32789294039afb8ab40d9bfedc
xuanweiH <525405891@qq.com>
2021-02-22 18:03:46 +0800

    出现最多的字符串以及次数 复习

--- algorithm/字符串/一个字符串出现最多的字符以及次数.js
+++ algorithm/字符串/一个字符串出现最多的字符以及次数.js
@@ -17,3 +17,26 @@ function getMostStr (str) {
    }
    return [s,max]
 }
+
+
+
+// practice
+
+function get(str) {
+  let obj = {}
+  let arr = str.split("")
+  let max = 1
+  let s = arr[0]
+  for(let i=0;i<arr.length-1;i++) {
+      if(obj[arr[i]]) {
+        let count = ++obj[arr[i]]
+        if(count>max) {
+            max = count
+            s = arr[i]
+        }
+      }else {
+        obj[arr[i]] = 1
+      }
+  }
+  return [s,max]
+}
