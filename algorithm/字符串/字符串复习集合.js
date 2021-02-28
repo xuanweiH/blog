c6790161bbc7381313ebe9d4a93f63a6155f099e
xuanweiH <525405891@qq.com>
2021-02-28 19:16:20 +0800

    复习单词翻转 回文字符串

---
+++ algorithm/字符串/字符串复习集合.js
@@ -0,0 +1,45 @@
+// 字符串复习之 判断回文字符串
+// api版本 
+// function isCircle(str) {
+//   return str.split('').rerverse().join('') === str
+// }
+// // 指针版本
+// function isCir(str) {
+//    if(typeof str !== 'string') return false
+//    let l = 0, r = str.length -1
+//    while(l<r) {
+//        if(str.charAt(l) !== str.charAt(r)) return false
+//        l++;
+//        r--;
+//    }
+//    return true
+// }
+
+// 字符串复习之 翻转字符串里面的单词
+// api+正则
+// 先去掉首尾空格 再把多空格转换一个空格 根据空格切分数组 翻转 再返回
+function reverseWord(str) {
+  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join()
+}
+
+//指针
+// 先去重 -> 当遇到' '时 且word存在的 把单词推入头部 否则把字母拼接上去 最后加入一次word 再进行翻转转字符串
+function reverseWord1(str) {
+  let l = 0, r = str.length -1
+  let queue = [] 
+  let word = ''
+  while(str.chatAt(l) === " ") l++
+  while(str.chatAt(r) === " ") r--
+  while(l<r) {
+     let chat = str.chatAt(l)
+      if(chat=== ' ' && word) {
+          queue.unshift(chat)
+          word = ''
+      }else if( chat !== ' ') {
+          word += chat
+      }
+      l++
+  }
+  queue.unshift(word)
+  return queue.reverse().join(' ')
+}
