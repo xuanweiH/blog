e9aee6ecf3e7cd9c1ea22702379b06bf5516b217
xuanweiH <525405891@qq.com>
2021-03-03 18:22:09 +0800

    字符串复习之 回文 相乘相加 重复次数 单词反转

--- algorithm/字符串/字符串复习集合.js
+++ algorithm/字符串/字符串复习集合.js
@@ -19,7 +19,7 @@
 // api+正则
 // 先去掉首尾空格 再把多空格转换一个空格 根据空格切分数组 翻转 再返回
 function reverseWord(str) {
-  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join()
+  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
 }
 
 //指针
@@ -44,6 +44,40 @@ function reverseWord1(str) {
   return queue.reverse().join(' ')
 }
 
+// 字符串相乘
+function strmul(str1, str2) {
+  // let i = str1.length-1, j = str2.length-1
+  let res = []
+  for(let i=0; i<str1.length-1;i++) {
+    let tmp1 = str1[str1.length-1-j] // 尾数1
+    for(let j=0; j<str2.length-1; j++) {
+      let tmp2 = str2[str2.length-1] // 尾数2
+      let pos = res[i+j]? res[i+j]+tmp1*tmp2 : tmp1*tmp2
+      res[i+j] = pos%10
+      pos>=10 && (res[i+j+1] = (res[i+j+1]? res[i+j+1]+Math.floor(pos/10): Math.floor(pos/10)))
+    }
+  }
+  return res.reverse().join('')
+}
+
+// 一个字符串出现最多的字符以及次数
+function max(str) {
+  let obj = {}, arr = str.split(''), max = 1
+  let s = arr[0]
+  for(let i=0;i<str.length-1;i++) {
+    if(obj[arr[i]]) {
+      let count = ++obj[arr[i]]
+      if(count > max) {
+        s = arr[i]
+        max = count
+      }
+    } else {
+      obj[arr[i]] = 0
+    }
+  }
+  return [s,max]
+}
+
 
 // 
 function isCir(str) {
@@ -62,7 +96,7 @@ function isCir1(str) {
 }
 // 
 function isword(str) {
-  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join()
+  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
 }
 // 
 function isword(str) {
@@ -83,4 +117,70 @@ function isword(str) {
   }
   queue.unshift(word)
   queue.reverse().join(' ')
+}
+
+// 回文字符串判断
+function iscir(str) {
+  return str === str.split('').reverse().join('')
+}
+function iscir1(str) {
+  let l = 0, r = str.length-1
+  if(typeof(str) !== 'string') return false
+  while(l<r) {
+    if(str.chatAt(l) !== str.chatAt(r)) return false
+    l++
+    r--
+  }
+  return true
+}
+// 单词翻转
+function word(str) {
+  return str.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
+}
+function word1(str) {
+  let l=0, r=str.length-1
+  let queue = []
+  let res = ''
+  while(str.chatAt(l) === ' ') l++
+  while(str.chatAt(r) === ' ') r--
+  while(l<r) {
+    let chat = str.chatAt(l)
+    if(chat ===' ' && word) {
+      queue.unshift(word)
+      word = ''
+    }else if(chat !== ' ') {
+      word+= chat
+    }
+    l++
+  }
+  queue.unshift(word)
+  return queue.reverse().join(' ')
+}
+// 字符串相加
+function strsum(str1, str2) {
+  let a = str1.length, b = str2.length
+  let tmp = 0, res = ''
+  while(a || b) {
+    a? tmp+= str[--a]: ""
+    b? tmp+= str[--b]: ""
+    res = tmp%10 + res 
+    if(tmp>9) tmp = 1
+    else tmp = 0
+  }
+  if(tmp) res = 1+res
+  return res
+}
+// 字符串相乘
+function multi(str1, str2) {
+  let res = []
+  for(let i=0;i<str1.length-1;i++) {
+    let tmp1 = str1[str1.length-1]
+    for(let j=0;j<str2.length-1;j++) {
+      let tmp2 = str2[str2.length-1-j]
+      let post = res[i+j]? res[i+j]+tmp1*tmp2: tmp1*tmp2
+      res[i+j] = post%10
+      post>=10 && (res[i+j+1] = (res[i+j+1]? res[i+j+1]+Math.floor(post/10):Math.floor(post/10) ))
+    }
+  }
+  return res.reverse().join('')
 }
