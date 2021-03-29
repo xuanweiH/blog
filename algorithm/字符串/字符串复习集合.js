98c67bf815c3935696cf1b3f06da234505458a49
xuanweiH <525405891@qq.com>
2021-03-29 18:45:09 +0800

    回文字符串复习

--- algorithm/字符串/字符串复习集合.js
+++ algorithm/字符串/字符串复习集合.js
@@ -5,18 +5,28 @@ function count(str) {
   let arr = str.split('')
   let obj = {}
   let s = arr[0]
-  let max = 1
-  for(let i=0; i<arr.length; i++) {
+  let max = 0
+  for(let i=0;i<arr.length-1;i++) {
     if(obj[arr[i]]) {
       let count = obj[arr[i]]++
-      if(count > max) {
+      if(count>max) {
         max = count
         s = arr[i]
       }
     } else {
-      obj[arr[i]] = 1
+      obj[arr[i]] = 0
     }
   }
-  return [max ,s]
-
+  return [s, max]
+}
+// 2. 判断回文字符串
+function isCir(str) {
+  let l = 0;
+  let r = str.length -1;
+  while(l<=r){
+    if(str.chatAt(l) !== str.chatAt(r)) return false
+    l++
+    r--
+  }
+  return true
 }
